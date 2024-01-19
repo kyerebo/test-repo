26713e50b793ebca72c156573073c2b6ce1a97c8
akyereboah_microsoft <akyereboah@microsoft.com>
2024-01-18 18:14:07 -0800

    Removing await to remove top-level restriction

--- 3pa-xss-test.js
+++ 3pa-xss-test.js
@@ -1,6 +1,6 @@
 // 3PA XSS test
 try {
-    const details = await navigator.acquisitionInfoProvider.getDetails();
+    const details = navigator.acquisitionInfoProvider.getDetails();
     console.log('API call successful:', details);
 } catch (error) {
     console.error('API call failed:', error);
